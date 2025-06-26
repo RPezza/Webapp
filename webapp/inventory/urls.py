3841b57dc1f27315d0b507a82d8f974fd48d1c53
RPezza <rowanperry05@googlemail.com>
2025-06-26 13:19:18 +0100

    Fix URL configuration and cleanup views

--- webapp/inventory/urls.py
+++ webapp/inventory/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
-
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('home/', views.home, name='home'),
     path('assets/', views.asset_list, name='asset_list'),
     path('book/', views.book_asset, name='book_asset'),
@@ -12,8 +11,4 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('register/', views.register, name='register'),
     path('edit_booking/<str:pk>/', views.edit_booking, name='edit_booking'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path('logout/', views.logout, name='logout'),
 ]
-
-
